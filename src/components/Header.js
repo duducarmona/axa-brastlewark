40b2720d14499f432dac95c17be7d1ada265b580
Edu Carmona <dudu.carmona@gmail.com>
2021-09-25 20:14:30 +0200

    Add button in Header to go back to the list.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,17 @@
 import './Header.css';
+import { Link } from 'react-router-dom';
 
-const Header = ({title}) => {
+const Header = ({ title }) => {
 	return (
 		<div className="Header">
+			{window.location.pathname !== '/' && (
+				<div className="arrow-icon-container">
+					<Link to="/">
+						<i className="material-icons arrow-icon">arrow_back_ios_new</i>
+					</Link>
+				</div>
+			)}
+
 			<h1>{title}</h1>
 		</div>
 	);
